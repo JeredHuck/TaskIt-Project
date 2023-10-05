74c81e6f0cff7757cb6b8f70a6a7421421d8f28a
JeredHuck <jeredhuck@gmail.com>
2023-10-05 14:10:38 -0500

    more things i did

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ProfileCardComponent } from './sidebar/profile-card/profile-card.component';
+import { VideoGameListComponent } from './video-game-list/video-game-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    ProfileCardComponent
+    ProfileCardComponent,
+    VideoGameListComponent
   ],
   imports: [
     BrowserModule
